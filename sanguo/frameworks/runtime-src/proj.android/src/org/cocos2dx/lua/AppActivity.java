8b288dbcd08b1cc2d3d2aff89ecc12b0308ab3f9
longlong <xian030711@163.com>
2017-02-24 10:09:11 +0800

    android工程调试

--- sanguo/frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
+++ sanguo/frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
@@ -26,7 +26,15 @@ THE SOFTWARE.
 package org.cocos2dx.lua;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 
 public class AppActivity extends Cocos2dxActivity{  
+    public Cocos2dxGLSurfaceView onCreateView() {
+        Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
+        // Tests should create stencil buffer
+        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
+        
+        return glSurfaceView;
+    }
 }
