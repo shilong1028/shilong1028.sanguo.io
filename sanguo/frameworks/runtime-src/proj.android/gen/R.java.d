8ef4020f05df8fab1a3291ba9ebab647c4c24d32
longlong <xian030711@163.com>
2017-02-28 11:05:50 +0800

    android架构（基本OK）

--- sanguo/frameworks/runtime-src/proj.android/gen/R.java.d
+++
@@ -1,6 +0,0 @@
-D:\MySanGuo\sanguo\frameworks\runtime-src\proj.android\gen\com\YingChuan\SanGuo\R.java \
- : D:\MySanGuo\sanguo\frameworks\runtime-src\proj.android\res\drawable-hdpi\icon.png \
-D:\MySanGuo\sanguo\frameworks\runtime-src\proj.android\res\drawable-ldpi\icon.png \
-D:\MySanGuo\sanguo\frameworks\runtime-src\proj.android\res\drawable-mdpi\icon.png \
-D:\MySanGuo\sanguo\frameworks\runtime-src\proj.android\res\values\strings.xml \
-D:\MySanGuo\sanguo\frameworks\runtime-src\proj.android\bin\AndroidManifest.xml \
