a9f08f2c4a5b36c604d14c91b8413294f7b237c9
longlong <xian030711@163.com>
2017-03-08 17:23:29 +0800

    login

---
+++ sanguo/src/Layer/Login/LoginLayer.lua
@@ -0,0 +1,161 @@
+
+
+local LoginLayer = class("LoginLayer", CCLayerEx)
+
+function LoginLayer:create()   --自定义的create()创建方法
+    --G_Log_Info("LoginLayer:create()")
+    local layer = LoginLayer.new()
+    return layer
+end
+
+function LoginLayer:onExit()
+    --G_Log_Info("LoginLayer:onExit()")
+
+end
+
+--初始化UI界面
+function LoginLayer:init()  
+    --G_Log_Info("LoginLayer:init()")
+    local csb = cc.CSLoader:createNode("csd/LoginLayer.csb")
+    self:addChild(csb)
+    csb:setContentSize(g_WinSize)
+    ccui.Helper:doLayout(csb)
+    --self:showInTheMiddle(csb)
+
+    local function BtnCallback(sender, eventType)
+        self:touchEvent(sender, eventType)
+    end
+    self.serverBtn = csb:getChildByName("serverBtn")    --选择服务器页面
+    self.serverBtn:addTouchEventListener(BtnCallback) 
+
+    self.startGameBtn = csb:getChildByName("startGameBtn")    --开始游戏
+    self.startGameBtn:addTouchEventListener(startGameBtnCallback)
+
+    local function TextFieldEvent(sender, eventType)
+        self:TextFieldEvent(sender, eventType)
+    end
+
+    --登录界面
+    self.loginNode = csb:getChildByName("nameBg")   
+    self.loginNode:setVisible(false)
+
+    --登录用户名
+    self.userNameStr = ""  
+    self.TextField_userName  = self.loginNode:getChildByName("TextField") 
+    self.TextField_userName:addEventListener(TextFieldEvent)
+
+    --登录密码
+    self.passWordStr = ""  
+    self.TextField_passWord  = self.loginNode:getChildByName("TextField_psw") 
+    self.TextField_passWord:addEventListener(TextFieldEvent)
+
+    self.loginBtn = self.loginNode:getChildByName("loginBtn")     --登录
+    self.loginBtn:addTouchEventListener(BtnCallback)
+
+    self.registerBtn = self.loginNode:getChildByName("registerBtn")    --注册界面
+    self.registerBtn:addTouchEventListener(BtnCallback)
+
+    --注册界面
+    self.registerNode = csb:getChildByName("registerBg")   
+    self.registerNode:setVisible(false)
+
+    --注册用户名
+    self.register_userNameStr = ""  
+    self.TextField_register_userName  = self.registerNode:getChildByName("TextField") 
+    self.TextField_register_userName:addEventListener(TextFieldEvent)
+
+    --注册密码
+    self.register_passWordStr = ""  
+    self.TextField_register_passWord  = self.registerNode:getChildByName("TextField_psw") 
+    self.TextField_register_passWord:addEventListener(TextFieldEvent)
+
+    --注册密码2
+    self.register_passWordStr2 = ""  
+    self.TextField_register_passWord2  = self.registerNode:getChildByName("TextField_psw2") 
+    self.TextField_register_passWord2:addEventListener(TextFieldEvent)
+
+    self.register_backBtn = self.registerNode:getChildByName("backBtn")     --注册返回
+    self.register_backBtn:addTouchEventListener(BtnCallback)
+
+    self.register_registerBtn = self.registerNode:getChildByName("registerBtn")    --注册用户
+    self.register_registerBtn:addTouchEventListener(BtnCallback)
+
+    --服务器界面
+    self.serverNode = csb:getChildByName("nameBg")   
+    self.serverNode:setVisible(false)
+
+    --服务器列表
+    self.serverListView  = self.serverNode:getChildByName("serverListView") 
+
+    self.lastSerBtn = self.serverNode:getChildByName("lastSerBtn")     --上一次登录服务器
+    self.lastSerBtn:addTouchEventListener(BtnCallback)
+
+    self.selectBtn = self.serverNode:getChildByName("selectBtn")     --选好了服务器
+    self.selectBtn:addTouchEventListener(BtnCallback)
+
+    self.server_backBtn = self.serverNode:getChildByName("backBtn")    --服务器返回
+    self.server_backBtn:addTouchEventListener(BtnCallback)
+end
+
+
+function LoginLayer:touchEvent(sender, eventType)
+    --G_Log_Info("LoginLayer:touchEvent")
+    if eventType == ccui.TouchEventType.ended then  
+        if sender == self.serverBtn then     --选择服务器页面
+
+        elseif sender == self.startGameBtn then   --开始游戏
+
+        elseif sender == self.loginBtn then   --登录
+
+        elseif sender == self.registerBtn then   --注册界面
+
+        elseif sender == self.register_backBtn then   --注册返回
+
+        elseif sender == self.register_registerBtn then   --注册用户
+
+        elseif sender == self.lastSerBtn then   --上一次登录服务器
+
+        elseif sender == self.selectBtn then   --选好了服务器
+
+        elseif sender == self.server_backBtn then   --服务器返回
+
+        end
+    end
+end
+
+function LoginLayer:TextFieldEvent(sender, eventType)
+    --G_Log_Info("LoginLayer:TextFieldEvent()")
+    if eventType == ccui.TextFiledEventType.attach_with_ime then   --获得输入焦点
+        --G_Log_Info("ATTACH_WITH_IME")
+    elseif eventType == ccui.TextFiledEventType.detach_with_ime then   --失去输入焦点
+        --G_Log_Info("DETACH_WITH_IME")
+    elseif eventType == ccui.TextFiledEventType.insert_text    --输入了文本
+        or eventType == ccui.TextFiledEventType.delete_backward   --删除了文字
+    then  
+        --G_Log_Info("INSERT_TEXT or DELETE_BACKWARD")
+        if sender == self.TextField_userName then  --登录用户名
+            self.userNameStr = self.TextField_userName:getString()
+        elseif sender == self.TextField_passWord then   --登录密码
+            self.passWordStr = self.TextField_passWord:getString()
+        elseif sender == self.TextField_register_userName then  --注册用户名
+            self.register_userNameStr = self.TextField_register_userName:getString()
+        elseif sender == self.TextField_register_passWord then  --注册密码
+            self.register_passWordStr = self.TextField_register_passWord:getString()
+        elseif sender == self.TextField_register_passWord2 then  --注册密码2
+            self.register_passWordStr2 = self.TextField_register_passWord2:getString()
+
+        elseif sender == self.TextField_userName then    --登录用户名
+            self.userNameStr = self.TextField_userName:getString()
+        elseif sender == self.TextField_passWord then   --登录密码
+            self.passWordStr = self.TextField_passWord:getString()
+        elseif sender == self.TextField_register_userName then  --注册用户名
+            self.register_userNameStr = self.TextField_register_userName:getString()
+        elseif sender == self.TextField_register_passWord then
+            self.register_passWordStr = self.TextField_register_passWord:getString()
+        end
+    end
+end
+
+
+
+return LoginLayer
